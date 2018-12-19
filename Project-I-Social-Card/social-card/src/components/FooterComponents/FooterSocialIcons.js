84ccd4cbea519b935dc5b4d1af89f821ebe5e4d7
joepound <14357797+joepound@users.noreply.github.com>
2018-12-18 21:20:17 -0700

    Added link functionality to social media icons in social card footer

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterSocialIcons.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterSocialIcons.js
@@ -8,13 +8,11 @@ import HeartsIcon from './../../images/heart.png';
 import MessageIcon from './../../images/message.png';
 
 const FooterSocialIcons = () => {
+  const socialIcons = [CommentsIcon, RetweetsIcon, HeartsIcon, MessageIcon]
+                        .map(icon => <a className="card__footer__social-links__icon" href="https://twitter.com/login?lang=en"><img src={icon}/></a>);
+
   return (
-    <div className="card__footer__social-links">
-      <img className="card__footer__social-links__icon" src={CommentsIcon}/>
-      <img className="card__footer__social-links__icon" src={RetweetsIcon}/>
-      <img className="card__footer__social-links__icon" src={HeartsIcon}/>
-      <img className="card__footer__social-links__icon" src={MessageIcon}/>
-    </div>
+    <div className="card__footer__social-links">{socialIcons}</div>
   );
 };
 
