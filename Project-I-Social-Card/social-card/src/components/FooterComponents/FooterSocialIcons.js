1f4c5f17bc20c58f59956f0423d55127f5dc95ad
joepound <14357797+joepound@users.noreply.github.com>
2018-12-18 22:32:21 -0700

    Added media queries (for responsive design) and other styling retouches

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterSocialIcons.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterSocialIcons.js
@@ -9,7 +9,7 @@ import MessageIcon from './../../images/message.png';
 
 const FooterSocialIcons = () => {
   const socialIcons = [CommentsIcon, RetweetsIcon, HeartsIcon, MessageIcon]
-                        .map(icon => <a className="card__footer__social-links__icon" href="https://twitter.com/login?lang=en"><img src={icon}/></a>);
+                        .map(icon => <a className="card__footer__social-links__icon" href="https://twitter.com/login?lang=en"><img src={icon}/> 0</a>);
 
   return (
     <div className="card__footer__social-links">{socialIcons}</div>
